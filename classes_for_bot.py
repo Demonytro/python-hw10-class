fdae7d49f734a52be1e378ca5674c8c52d69743f
Dmytro Oseledko <d.oseledko@outlook.com>
2023-01-02 19:04:36 +0200

    without ADD

--- classes_for_bot.py
+++ classes_for_bot.py
@@ -22,19 +22,23 @@ class Phone(Field):
 
     def __init__(self, phone):
         self.value = phone
-        print(f"phone - {self.value}")
+        print(f"phone - {self.value}   {type(self.value)}")
 
     def __repr__(self):
         return self.value
 
 
-class AddressBook(UserDict):
+class AddressBook(UserDict):     #  , Record
+
+    # def __init__(self):
+    #     super().
 
     def add_record(self, record):
+        print(f"record111 - {record.name.value}   {type(record.name.value)}")
         self.data[record.name.value] = record
 
 
-class Record(Name, Phone):
+class Record:
     """Класс Record, который 
     отвечает за логику добавления/удаления/редактирования 
     необязательных полей 
@@ -46,12 +50,14 @@ class Record(Name, Phone):
 
     Record реализует методы для добавления/удаления/редактирования объектов Phone."""
 
-    def __init__(self, new_name):
-        self.name = Name(new_name)
+    def __init__(self, new_name, new_phone):
+        self.name = new_name
         self.phones = []
+        print(f"phone111 - {new_phone}   {type(phone.value)}")
+        print(f"phone111 - {self.name.value}   {type(self.name.value)}")
 
-    def add_phone(self, new_phone):        
-        self.phones.append(Phone(new_phone))
+    # def add_phone(self, new_phone):        
+        self.phones.append(new_phone)
 
     def change_phone(self, old_phone, new_phone):        
         for phone in self.phones:
@@ -73,4 +79,16 @@ class Record(Name, Phone):
 
 
 if __name__ == '__main__':
-    pass
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+    
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+    
+    print('All Ok)')
